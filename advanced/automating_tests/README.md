52223c6ab9530de922839509746897f33962e450
hyunjin.kim <roykim0823@gmail.com>
2022-05-23 14:16:48 +0900

    remove unnecessary files
    Signed-off-by: hyunjin.kim <roykim0823@gmail.com>

---
+++ advanced/automating_tests/README.md
@@ -0,0 +1,10 @@
+# Automating Tests
+
+1. Test Functions
+
+2. assert! macro
+
+3. Custom failure messages
+
+4. 
+8. Exercise: Write automated tests
--- advanced/automating_tests/test_fucntions/target/rls/debug/deps/test_fucntions-b914237c40ab41d7.d
+++
@@ -1,5 +0,0 @@
-/home/roy/code/Rust/advanced/automating_tests/test_fucntions/target/rls/debug/deps/test_fucntions-b914237c40ab41d7.rmeta: src/lib.rs
-
-/home/roy/code/Rust/advanced/automating_tests/test_fucntions/target/rls/debug/deps/test_fucntions-b914237c40ab41d7.d: src/lib.rs
-
-src/lib.rs:
